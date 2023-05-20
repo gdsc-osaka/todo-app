75324a67ed27a116dd456308732cbe5e00ec4fff
Harineko0 <harineko0927@gmail.com>
2023-05-20 23:11:28 +0900

    [Add] json_annotation, build_runner, json_serializable 追加.

---
+++ lib/theme/input_decorations.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const filledTextFieldDecoration = InputDecoration(
+    // hoverColor: Color(0xFFFFFFFF),
+    // prefixIconColor: Color(0xFF3C3C3C),
+    contentPadding: EdgeInsets.only(top: 0, left: 10),
+    // isDense: true,
+    filled: false,
+    border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+        borderSide: BorderSide(
+          width: 2,
+          style: BorderStyle.solid,
+        )));
