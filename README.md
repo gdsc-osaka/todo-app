114826a926d75b6fae294ebfbf9fd7f9498eafda
Harineko0 <harineko0927@gmail.com>
2023-05-20 22:15:57 +0900

    [Add] Google認証を追加.

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
-# todo_app
+# To-Do
 
-A new Flutter project.
+## DB設計
+users/
+├ userA
+│   └ tasks/
+│       ├ taskA
+│       └ taskB
 
-## Getting Started
+User
+- createdAt Timestamp
+- updatedAt Timestamp
 
-This project is a starting point for a Flutter application.
+Task
+- createdAt Timestamp
+- updatedAt Timestamp
+- title String
+- description String
+- until Timestamp
+- images String[]
+- status String(enum: undone, completed)
 
-A few resources to get you started if this is your first Flutter project:
+## Google認証
+https://zenn.dev/kazutxt/books/flutter_practice_introduction/viewer/30_chapter4_authentication#google%E3%82%A2%E3%82%AB%E3%82%A6%E3%83%B3%E3%83%88%E3%81%AB%E3%82%88%E3%82%8B%E8%AA%8D%E8%A8%BC
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+keytool
+`keytool -genkey -v -keystore .android/debug.keystore -alias androiddebugkey -keyalg RSA -validity 10000 -dname "CN=Android Debug,O=Android,C=US"`
+`keytool -list -v -keystore .android/debug.keystore`
--- lib/features/auth/auth_page.dart
+++ lib/features/auth/auth_page.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 import 'package:todo_app/components/check_text.dart';
 import 'package:todo_app/components/text_divider.dart';
 import 'package:todo_app/features/auth/email_login_page.dart';
+import 'package:todo_app/features/home/home_page.dart';
 
 import 'auth_providers.dart';
 import 'email_signup_page.dart';
@@ -19,8 +23,42 @@ class AuthPage extends ConsumerWidget {
     final theme = Theme.of(context);
     final text = theme.textTheme;
 
+    googleSignUp() async {
+      final messenger = ScaffoldMessenger.of(context);
+      context.loaderOverlay.show();
+
+      final account = await GoogleSignIn(scopes: [
+        'email',
+        'https://www.googleapis.com/auth/contacts.readonly',
+      ]).signIn();
+
+      if (account == null) {
+        context.loaderOverlay.hide();
+        messenger.showSnackBar(const SnackBar(content: Text("認証に失敗しました")));
+        return;
+      }
+
+      final auth = await account.authentication;
+      final credential = GoogleAuthProvider.credential(idToken: auth.idToken, accessToken: auth.accessToken);
+
+      final userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+      final user = userCredential.user;
+
+      if (user == null) {
+        context.loaderOverlay.hide();
+        messenger.showSnackBar(const SnackBar(content: Text("認証に失敗しました")));
+        return;
+      } else {
+        context.loaderOverlay.hide();
+        if (context.mounted) {
+          context.go(HomePage.name);
+        }
+        return;
+      }
+    }
+
     final signUpButtons = [
-      SizedBox(width: double.infinity, child: FilledButton(onPressed: () {}, child: Text("Googleで登録"))),
+      SizedBox(width: double.infinity, child: FilledButton(onPressed: googleSignUp, child: const Text("Googleで登録"))),
       const SizedBox(height: 16),
       SizedBox(
           width: double.infinity,
@@ -28,7 +66,7 @@ class AuthPage extends ConsumerWidget {
     ];
 
     final logInButtons = [
-      SizedBox(width: double.infinity, child: FilledButton(onPressed: () {}, child: const Text("Googleでログイン"))),
+      SizedBox(width: double.infinity, child: FilledButton(onPressed: googleSignUp, child: const Text("Googleでログイン"))),
       const SizedBox(height: 16),
       SizedBox(
           width: double.infinity,
