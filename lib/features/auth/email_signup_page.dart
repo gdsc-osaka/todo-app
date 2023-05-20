e575d6f056b9f9e4e945da75d33b5eec733ac581
Harineko0 <harineko0927@gmail.com>
2023-05-20 18:18:30 +0900

    [Add] EmailSignUpPage を追加. email_validator を pubspec.yaml に追加.

---
+++ lib/features/auth/email_signup_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class EmailFormPage extends StatefulWidget {
+  const EmailFormPage({super.key});
+
+  @override
+  State<EmailFormPage> createState() => _EmailFormPageState();
+}
+
+class _EmailFormPageState extends State<EmailFormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: ,
+        ),
+      ),
+    );
+  }
+}
