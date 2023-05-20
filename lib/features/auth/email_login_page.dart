79925d8fa2731694f3968fba920fad5d60647c59
Harineko0 <harineko0927@gmail.com>
2023-05-20 19:31:03 +0900

    [Add] EmailLoginPage を追加.

---
+++ lib/features/auth/email_login_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'email_form.dart';
+
+class EmailLoginPage extends ConsumerStatefulWidget {
+  const EmailLoginPage({Key? key}) : super(key: key);
+
+  @override
+  EmailLoginPageState createState() => EmailLoginPageState();
+}
+
+class EmailLoginPageState extends ConsumerState<EmailLoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _email = "";
+  String _password = "";
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width * 0.6;
+    final theme = Theme.of(context);
+    final text = theme.textTheme;
+
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: width,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("メールアドレスでログイン", style: text.headlineSmall),
+                const SizedBox(height: 60),
+                EmailForm(
+                    key: _formKey,
+                    email: _email,
+                    password: _password,
+                    onChangeEmail: (value) => setState(() {
+                          _email = value;
+                        }),
+                    onChangePassword: (value) => setState(() {
+                          _password = value;
+                        })),
+                const SizedBox(height: 28),
+                FilledButton(
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        // 入力データが正常な場合
+                      }
+                    },
+                    child: const Text("ログイン"))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
