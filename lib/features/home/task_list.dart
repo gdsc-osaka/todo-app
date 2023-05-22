aa833293fc350fc18c2b92801a26ad57153e60d2
Harineko0 <harineko0927@gmail.com>
2023-05-22 22:09:34 +0900

    [Add] Add intl, TaskList, TaskListItem, DoneTaskList, DoneTaskListItem

---
+++ lib/features/home/task_list.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../model/task.dart';
+
+class DoneTaskListItem extends StatelessWidget {
+  const DoneTaskListItem({super.key, required this.task});
+
+  final Task task;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(Icons.check),
+        Text(task.title, style: const TextStyle(decoration: TextDecoration.lineThrough)),
+        OutlinedButton(onPressed: () {}, child: const Text("戻す"))
+      ],
+    );
+  }
+}
+
+class DoneTaskList extends StatelessWidget {
+  const DoneTaskList({super.key, required this.tasks});
+
+  final List<Task> tasks;
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: const Text("完了済みタスク"),
+      children: tasks
+          .map((task) => Padding(padding: const EdgeInsets.only(left: 12), child: DoneTaskListItem(task: task)))
+          .toList(),
+    );
+  }
+}
+
+class TaskListItem extends StatefulWidget {
+  const TaskListItem({super.key, required this.task});
+
+  final Task task;
+
+  @override
+  State<TaskListItem> createState() => _TaskListItemState();
+}
+
+class _TaskListItemState extends State<TaskListItem> {
+  bool isDone = false;
+
+  static final DateFormat _formatter = DateFormat('yyyy/MM/dd');
+
+  @override
+  Widget build(BuildContext context) {
+    final task = widget.task;
+    final images = task.images;
+    final photo = images.isEmpty ? const SizedBox() : Image.network(images[0]);
+    final until = task.until.toDate();
+
+    return Card(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Checkbox(value: isDone, onChanged: (value) {}),
+          Expanded(
+            child: Column(
+              children: [Text(widget.task.title), Chip(label: Text(_formatter.format(until)))],
+            ),
+          ),
+          photo
+        ],
+      ),
+    );
+  }
+}
+
+class TaskList extends StatelessWidget {
+  const TaskList({super.key, required this.tasks});
+
+  final List<Task> tasks;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) => TaskListItem(task: tasks[index]),
+      itemCount: tasks.length,
+    );
+  }
+}
